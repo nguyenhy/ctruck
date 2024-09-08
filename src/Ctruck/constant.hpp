6db5be41239a22d6782637df1708cf048e9b8f3b
nguyenhy <nguyenhy311@gmail.com>
2024-09-08 17:39:57 +0700

    fix:  define guard macro

---
+++ src/Ctruck/constant.hpp
@@ -0,0 +1,10 @@
+#ifndef _INTERNAL_CTRUCK_CONSTANT
+#define _INTERNAL_CTRUCK_CONSTANT
+
+#define SCREEN_WIDTH 800
+#define SCREEN_HEIGHT 450
+
+#define FPS 60
+
+#define FALL_TOLERANCE 0.001 // s
+#endif
